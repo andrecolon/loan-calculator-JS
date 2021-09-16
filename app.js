2db4aaeea4e4119741b9ea3d1275c4d7014776c3
Andre Colon <andrecolon@gmail.com>
2021-09-16 06:52:53 -0700

    rolled back bootstrap version -for cleaner UI

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-alert("1")
+// Listen for sumit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+function calculateResults(e){
+    const uiAmount = document.getElementById('amount');
+    const interest - getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+
+    const principal = parseFloat(uiAmount.value);
+    const calcInterest = parseFloat(interset.value) / 100 / 12;
+    const calcPayments = parseFloat(years.value) * 12;
+
+    // Compute monthly payments
+
+    const x = Math.pow(1 + calcInterest, calcPayments);
+    const monthly = (principal*x*calcInterest)/(x-1)
+    e.preventDefault()
+}
